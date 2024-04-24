d253ece7ace65a968874260b25f7d9fe88db075a
Emmanuel <67012829+Plaza99@users.noreply.github.com>
2024-04-24 18:05:17 +0200

    Gallery implemented
    - Added exIfInterface dependency for read GeoLocation
    - Implemented Gallery UI and method to show images and read location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation("androidx.exifinterface:exifinterface:1.3.7")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/it/unipi/mobile/vineplanthealthapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/it/unipi/mobile/vineplanthealthapp/ui/gallery/GalleryFragment.kt
@@ -4,39 +4,162 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import it.unipi.mobile.vineplanthealthapp.databinding.FragmentGalleryBinding
+import it.unipi.mobile.vineplanthealthapp.R
+import android.Manifest
+import android.widget.GridView
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.widget.Toast
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Environment
+import android.widget.ImageView
+import android.graphics.Bitmap
+import android.widget.BaseAdapter
+import android.content.Context
+import android.media.ExifInterface
+
+
+data class Image(val bitmap: Bitmap, val uri: Uri)
+private const val MY_PERMISSIONS_REQUEST_READ_MEDIA_IMAGES = 1
 
 class GalleryFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var galleryGridView: GridView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val view = inflater.inflate(R.layout.fragment_gallery, container, false)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        // Retrieve the GridView from the layout
+        galleryGridView = view.findViewById(R.id.gallery_grid_view)
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val checkMediaPermission = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.READ_MEDIA_IMAGES
+        )
+        val checkLocationPermission = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_MEDIA_LOCATION
+        )
+
+        if (checkMediaPermission != PackageManager.PERMISSION_GRANTED && checkLocationPermission != PackageManager.PERMISSION_GRANTED) {
+            val permissionMedia = ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                Manifest.permission.READ_MEDIA_IMAGES
+            );
+            val permissionLocation = ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                Manifest.permission.ACCESS_MEDIA_LOCATION
+            );
+            // Permission is not granted
+            if (!permissionMedia || !permissionLocation) {
+                Toast.makeText(
+                    requireContext(),
+                    "Gallery and Location permissions needed!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                // Request the permission
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(
+                        Manifest.permission.READ_MEDIA_IMAGES,
+                        Manifest.permission.ACCESS_MEDIA_LOCATION
+                    ),
+                    MY_PERMISSIONS_REQUEST_READ_MEDIA_IMAGES
+                )
+                loadImages()
+                showLocation()
+            }
         }
-        return root
+        else{
+            loadImages()
+            showLocation()
+        }
+        return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun loadImages() {
+        val images = mutableListOf<Image>()
+        val picturesDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        val imageFiles = picturesDirectory.listFiles()
+
+        if (imageFiles != null) {
+            for (file in imageFiles) {
+                if (file.isFile && (file.path.endsWith(".jpg") || file.path.endsWith(".png"))) {
+                    val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+                    val uri = Uri.fromFile(file)
+                    images.add(Image(bitmap, uri))
+                }
+            }
+        }
+
+        val imageAdapter = ImageAdapter(requireContext(), images)
+        galleryGridView.adapter = imageAdapter
+    }
+
+    private fun showLocation(){
+        val imagesList = loadImages()
+        if (listOf(imagesList).isEmpty()) {
+            Toast.makeText(requireContext(), "No images found", Toast.LENGTH_SHORT).show()
+        }
+
+        galleryGridView.setOnItemClickListener { parent, view, position, id ->
+            val image = galleryGridView.adapter.getItem(position) as Image
+            val geoLocation = image.uri.path?.let { getGeoLocation(it) }
+            if (geoLocation != null)
+                Toast.makeText(
+                    requireContext(),
+                    "Lat: ${geoLocation.first}, Lon: ${geoLocation.second}",
+                    Toast.LENGTH_SHORT
+                ).show()
+        }
+    }
+
+    class ImageAdapter(private val context: Context, var images: List<Image>) : BaseAdapter() {
+        override fun getCount(): Int {
+            return images.size
+        }
+
+        override fun getItem(position: Int): Any {
+            return images[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val imageView: ImageView
+            if (convertView == null) {
+                imageView = ImageView(context)
+                imageView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,1000)
+                imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+                imageView.setPadding(10, 10, 10, 10)
+            } else {
+                imageView = convertView as ImageView
+            }
+
+            imageView.setImageBitmap(images[position].bitmap)
+            return imageView
+        }
+    }
+
+    fun getGeoLocation(imagePath: String): Pair<Double, Double>? {
+        val exifInterface = ExifInterface(imagePath)
+
+        val latLong = FloatArray(2)
+        val hasLatLong = exifInterface.getLatLong(latLong)
+        if (hasLatLong) {
+            return Pair(latLong[0].toDouble(), latLong[1].toDouble())
+        }
+        Toast.makeText(requireContext(), "No geolocation found for this image", Toast.LENGTH_SHORT).show()
+        return null
     }
 }
