056ffe57ae1f5b3e4e656d539c934ebc04e7af02
Salvatore Patisso <120101001+Salion0@users.noreply.github.com>
2024-05-03 11:22:37 +0200

    Added Test class for InferenceActivity

--- app/src/androidTest/java/it/unipi/mobile/vineplanthealthapp/InferenceActivityTest.kt
+++ app/src/androidTest/java/it/unipi/mobile/vineplanthealthapp/InferenceActivityTest.kt
@@ -1,5 +1,8 @@
 package it.unipi.mobile.vineplanthealthapp
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.Image
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +10,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.tensorflow.lite.support.image.TensorImage
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +18,21 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class InferenceActivityTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("it.unipi.mobile.vineplanthealthapp", appContext.packageName)
     }
+
+    @Test
+    fun startInferenceActivityWithIntentUsingByteTAG(){
+        //TODO pick the first image from the galleryù
+
+        val bitmapImage : Bitmap = BitmapFactory.decodeFile()
+
+
+    }
+
 }
